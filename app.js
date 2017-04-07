b4834e673f5088bab1d831f5f8ea43d01fee95dd
Brian McFarlane <bmcfarlane82@gmail.com>
2017-04-07 16:45:21 -0700

    sumarray is working

--- app.js
+++ app.js
@@ -85,12 +85,18 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2,3,4]; //eslint-disable-line
 
 function sumArray(testArray){ //eslint-disable-line
-
+  var answers = []
+  var arraySum;
+  arraySum = sum(testArray[0], testArray[1]);
+  arraySum = sum(arraySum[0], testArray[2]);
+  answers[0] = arraySum[0];
+  answers[1] = (testArray[0] + ',' + testArray[1] + ',' + testArray[2] + ' was passed in as an array of numbers, and 9 is their sum.')
+  return answers
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
