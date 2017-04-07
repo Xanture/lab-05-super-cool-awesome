ea62b9e77d807314816ddf81c3826fba71d1f60e
Brian McFarlane <bmcfarlane82@gmail.com>
2017-04-07 15:55:24 -0700

    sum and prod complete

--- app.js
+++ app.js
@@ -54,11 +54,20 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a,b,c){ //eslint-disable-line
-
+  var answers = [];
+  var sumnum = sum(a, b);
+  sumnum = sum(sumnum[0], c);
+  var prodnum = multiply(a, b);
+  prodnum = multiply(prodnum[0], c);
+  answers[0] = sumnum[0];
+  answers[1] = prodnum[0];
+  answers[2] = (a + ' and ' + b + ' and ' + c + ' sum to ' + sumnum[0] + '.')
+  answers[3] = ('The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + prodnum[0] +'.')
+  return answers
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
